1e449621809691f7283d468a1b2e47ba66fd0c5c
tom <344924008@qq.com>
2025-07-04 15:44:17 +0800

    feat(locale_text): 添加字符串本地化扩展方法
    实现String的扩展方法lt()，用于根据上下文、页面和类别获取本地化文本
    支持通过参数或上下文获取页面、类别和语言设置
    当页面未设置时抛出异常提示需要包裹在LocaleTextProvider中

--- lib/src/locale_text_ext.dart
+++ lib/src/locale_text_ext.dart
@@ -1,3 +1,34 @@
 part of '../locale_text.dart';
 
-extension LocalTextExtString on String {}
+extension LocalTextExtString on String {
+  /// 本地化文本
+  ///
+  /// [args] 文本参数
+  /// [context] 上下文
+  /// [page] 页面为空的话，组件必须包裹在LocaleTextProvider中或者LocaleTextApp中
+  /// [category] 类别
+  /// [locale] 本地化
+  String lt(List<String>? args,
+      {BuildContext? context, String? page, String? category, Locale? locale}) {
+    final LocaleTextProvider? localeTextProvider =
+        context == null ? null : LocaleTextProvider.of(context);
+    final InheritedLocaleTextApp? localeTextApp =
+        context == null ? null : LocaleTextApp.of(context);
+
+    final finalPage = page ?? localeTextProvider?.page ?? localeTextApp?.page;
+    final finalLocale = locale ?? localeTextApp?.locale;
+    final finalCategory =
+        category ?? localeTextProvider?.category ?? localeTextApp?.category;
+    if (finalPage == null) {
+      throw Exception(
+          "page is null, please wrap widget with LocaleTextProvider or LocaleTextApp");
+    }
+    final str = LocaleTextParser.parseText(
+        page: finalPage,
+        category: finalCategory,
+        key: this,
+        locale: finalLocale,
+        args: args);
+    return str;
+  }
+}
